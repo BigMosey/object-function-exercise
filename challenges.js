9488d2498eec502b04911852504bed9483a85eff
BigMosey <willy.salas6996@gmail.com>
2023-03-30 20:35:46 -0700

    fixed errors

--- challenges.js
+++ challenges.js
@@ -1,21 +1,34 @@
 // Create a variable called myName and assign it your name as a string.
-
+let myName ="Willy"
 // Create an array called numbers and add five numbers to it.
 
 // Create a variable called myAge and assign it your age as a number.
-
+var myAge = 24
 // Create an object called person with properties name, age, and gender.
-
+const person ={
+    name:"Willy",
+    age:"24",
+    gender:"male"
+}
 // Create a variable called isRaining and assign it a boolean value based on whether it is currently raining or not.
-
+var isRaining 
 // Create a function called greet that takes a name as a parameter and returns a greeting message using template literals.
+function greeting(name){
 
+    return `Hello,${name}! `
+}
 // Create a function called multiply that takes two numbers as parameters and returns their product.
-
+function mult(a,b){
+    return a * b;
+}
 // Create a function called divide that takes two numbers as parameters and returns their quotient.
-
+function division(a,b){
+    return a / b;
+}
 // BONUS
 // Create a function called isEven that takes a number as a parameter and returns true if the number is even, false otherwise.
-
+function isEven(a){
+ let a = true
+}
 
 
